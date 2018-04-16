118167554df606dce1a05535d1676cc4bf61bc27
Stefan <s.j.grimminck@gmail.com>
2018-04-16 11:51:49 +0200

    Add files via upload

---
+++ assignment4/UM10326.h
@@ -0,0 +1,13 @@
+#ifndef PCA
+#define PCA
+
+/* Values to select certain registers to write to */
+#define PWM1_CONTR_REG			0x4005C000
+#define PWM2_CONTR_REG			0x4005C004
+#define CLK_FREQ				32000		
+
+#define PWM_EN 				0x80000000
+#define PWM_FREQ			0x0000FF00
+
+
+#endif
